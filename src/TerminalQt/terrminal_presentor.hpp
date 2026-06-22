094ca73a888a2f429a63c134af070d5da0bb82d7
raulcs16 <sc.raul.18@gmail.com>
2026-06-22 06:09:40 -0700

    .

--- src/TerminalQt/terrminal_presentor.hpp
+++ src/TerminalQt/terrminal_presentor.hpp
@@ -14,7 +14,11 @@ class TerminalPresentor : public QObject public TerminalOut {
     QML_UNCREATABLE("Created by ui context")
 public:
     TerminalPresentor() {}
-    void presentLine(const std::string &line) {}
+    void presentLine(const std::string &line) {
+        size_t id = m_idx++;
+        m_lines[id] = QString(line);
+        emit lineAdded(id);
+    }
     void presentList(std::vector<std::string> list) override {
         m_list.clear();
         for (auto &item : list) {
@@ -27,12 +31,19 @@ public:
         m_suggestion = QString(suggestion);
         emit suggestionChanged();
     }
+    QString suggestion() const { return m_suggestion; }
+    std::vector<QString> list() const { return m_list; }
+    QString line(size_t id) const { return m_lines[id]; }
+
+
 signals:
     void suggestionChanged();
     void listChanged();
+    void lineAdded(size_t id);
 
 private:
     QString m_suggestion;
     std::vector<QString> m_list;
     std::unordered_map<size_t, QString> m_lines;
+    size_t m_idx = 1;
 };
